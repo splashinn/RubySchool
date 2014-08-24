80f99e66353e2401ecef522173a982b5afb49d80
Kyle Maune <kmaune71@gmail.com>
2014-08-24 12:07:05 -0700

    Initial commit, lesson 1
    Brief Ruby introduction. Covers variables and printing output to the
    console.

---
+++ ruby_intro.rb
@@ -0,0 +1,19 @@
+# Lesson 1: Ruby Introduction
+
+# Setting variables in Ruby is super simple. For example:
+my_name =("Kyle")
+
+# Whenever you call my_name, Kyle will be the result.
+# To print the output to the screen, you could use the 'print' method, or the 'puts' method.
+# Try them both out to see the difference between them.
+# To run this from the command line, navigate to the directory of the saved Ruby file, and type ruby your_filename.rb, then press enter.
+
+puts(my_name)
+print(my_name)
+
+# Notice that the 'puts' method inserts a new line after the output, while 'print' does not.
+# Lets practice printing output to the screen!
+# ***The parentheses can be left off as well, Ruby doesn't care! I use them because I prefer them.***
+
+# Exercise 1: Set a variable 'name' to equal your name. Print it to the console. Don't be afraid to experiment!!
+# Exercise 2: Create a few variables. See if you can figure out how to string them together in the output. Hint: +
